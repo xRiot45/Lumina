1c4f6d62689dcbb95a4a29abeabd80b5f463ac0c
xRiot45 <tomasalberto527@gmail.com>
2026-03-11 18:46:04 +0700

    refactor: refactoring code

--- apps/carts-service/src/app/carts/carts.controller.ts
+++ apps/carts-service/src/app/carts/carts.controller.ts
@@ -1,3 +1,4 @@
+import { CARTS_COMMAND_PATTERN } from '@lumina/shared-common';
 import { AddToCartPayloadDto, CartResponseDto, EnrichedCartItemResponseDto } from '@lumina/shared-dto';
 import type {
     IDeleteCartPayload,
@@ -14,27 +15,27 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class CartsController {
     constructor(private readonly cartsService: CartsService) {}
 
-    @MessagePattern({ cmd: 'add_to_cart' })
+    @MessagePattern(CARTS_COMMAND_PATTERN.ADD_TO_CART)
     async addToCart(@Payload() payload: AddToCartPayloadDto): Promise<CartResponseDto> {
         return await this.cartsService.addToCart(payload.userId, payload.data);
     }
 
-    @MessagePattern({ cmd: 'get_cart' })
+    @MessagePattern(CARTS_COMMAND_PATTERN.GET_CART)
     async getCart(@Payload() payload: IGetCartPayload): Promise<IPaginatedResponse<EnrichedCartItemResponseDto>> {
         return await this.cartsService.getCart(payload.userId, payload.query);
     }
 
-    @MessagePattern({ cmd: 'delete_item_from_cart' })
+    @MessagePattern(CARTS_COMMAND_PATTERN.DELETE_ITEM_FROM_CART)
     async deleteItemFromCart(@Payload() payload: IDeleteItemFromCartPayload): Promise<{ success: boolean }> {
         return await this.cartsService.deleteItemFromCart(payload.userId, payload.cartItemId);
     }
 
-    @MessagePattern({ cmd: 'delete_cart' })
+    @MessagePattern(CARTS_COMMAND_PATTERN.DELETE_CART)
     async deleteCart(@Payload() payload: IDeleteCartPayload): Promise<{ success: boolean }> {
         return await this.cartsService.deleteCart(payload.userId, payload.cartId);
     }
 
-    @MessagePattern({ cmd: 'update_item_quantity' })
+    @MessagePattern(CARTS_COMMAND_PATTERN.UPDATE_ITEM_QUANTITY)
     async updateItemQuantity(@Payload() payload: IUpdateCartItemPayload) {
         return await this.cartsService.updateItemQuantity(payload.userId, payload.cartItemId, payload.quantity);
     }
